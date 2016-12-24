c0d298f66088466c3566dbaf79a2eb7c7bff0b2c
Kolland <koland.my@gmail.com>
2016-12-24 11:02:36 +0200

    state on 24.12

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
@@ -14,18 +19,34 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new NpmInstallPlugin()
   ],
-  module: { //Обновлено
-    loaders: [ //добавили babel-loader
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
+    loaders: [
       {
         loaders: ['babel-loader'],
         include: [
-        path.resolve(__dirname, "src"),
-      ],
+          path.resolve(__dirname, "src"),
+        ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   }
 }
